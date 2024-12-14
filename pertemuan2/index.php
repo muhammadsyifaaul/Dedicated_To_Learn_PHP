a1adfc0341e34386758a6e637ed2d72ae4c0496f
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-12-14 16:31:50 +0700

    sintaks_dasar_php

---
+++ pertemuan2/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// echo "Hello World";
+
+$nama = "John Doe";
+$umur = 2004 + 20 - 2024 +20;
+
+echo "Nama saya adalah " . $nama . " dan umur saya adalah " . $umur . " tahun.";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $nama . " " . $umur; ?></h1>
+</body>
+</html>
